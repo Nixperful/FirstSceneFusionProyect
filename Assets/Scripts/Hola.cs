2a8b76912aa38dba4eb4164ed1c28ce48a0e63e7
JuanNicolasGomez <nicolas.gm243@gmail.com>
2020-08-09 18:44:09 -0500

    [change] movement script

--- Assets/Scripts/Hola.cs
+++ Assets/Scripts/Hola.cs
@@ -5,35 +5,42 @@ using UnityEngine;
 public class Hola : MonoBehaviour
 {
     
-    public float speed = 6.0F;
-    public float jumpSpeed = 8.0F; 
-    public float gravity = 20.0F;
-    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = gameObject.AddComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        CharacterController controller = GetComponent<CharacterController>();
-         // is the controller on the ground?
-         if (controller.isGrounded) {
-             //Feed moveDirection with input.
-             moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0 );
-             moveDirection = transform.TransformDirection(moveDirection);
-             //Multiply it by speed.
-             moveDirection *= speed;
-             //Jumping
-             if (Input.GetButton("Jump"))
-                 moveDirection.y = jumpSpeed +10;
-             
-         }
-         //Applying gravity to the controller
-         moveDirection.y -= gravity * Time.deltaTime;
-         //Making the character move
-         controller.Move(moveDirection * Time.deltaTime);
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
