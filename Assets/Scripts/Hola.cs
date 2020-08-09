d13f4817758ce8b9e7b87a232a92fd47ab90afb1
JuanNicolasGomez <nicolas.gm243@gmail.com>
2020-08-09 17:51:01 -0500

    add Hola movement script

---
+++ Assets/Scripts/Hola.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hola : MonoBehaviour
+{
+    
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F; 
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+         // is the controller on the ground?
+         if (controller.isGrounded) {
+             //Feed moveDirection with input.
+             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+             moveDirection = transform.TransformDirection(moveDirection);
+             //Multiply it by speed.
+             moveDirection *= speed;
+             //Jumping
+             if (Input.GetButton("Jump"))
+                 moveDirection.y = jumpSpeed;
+             
+         }
+         //Applying gravity to the controller
+         moveDirection.y -= gravity * Time.deltaTime;
+         //Making the character move
+         controller.Move(moveDirection * Time.deltaTime);
+    }
+}
